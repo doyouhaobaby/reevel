1bff4ccf79e39d4a0c56c09b4b40314905f1a64c
Xiangmin Liu <635750556@qq.com>
2019-02-17 19:24:13 +0800

    多线程

--- bookdao/b116/src/main.rs
+++ bookdao/b116/src/main.rs
@@ -1,3 +1,31 @@
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    //println!("Hello, world!");
+
+    let child = thread::spawn(move || {
+       for _ in 0..20 {
+           unsafe_seq();
+           unsafe {
+              println!("child: {}", v);
+           }
+       } 
+    });
+
+    for _ in 0..20 {
+        unsafe_seq();
+        unsafe {
+            println!("main: {}", v);
+        }
+    }  
 }
+
+static mut v: i32 = 0;
+
+fn unsafe_seq() -> i32 {
+    unsafe {
+        v += 1;
+        v
+    }
+}
+
