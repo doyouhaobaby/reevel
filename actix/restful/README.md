2fad6f134d20d0719374810341cdc0c3045e6eff
Xiangmin Liu <635750556@qq.com>
2019-03-19 16:13:40 +0800

    feat: 加入新的提取器

--- actix/restful/README.md
+++ actix/restful/README.md
@@ -2,7 +2,7 @@
 
 ## 监听
 
-systemfd --no-pid -s http::8888 -- cargo watch -x run
+systemfd --no-pid -s http::9888 -- cargo watch -x run
 
 ## 格式化 
 
--- actix/restful/src/main.rs
+++ actix/restful/src/main.rs
@@ -4,11 +4,16 @@ extern crate serde;
 extern crate serde_json;
 #[macro_use]
 extern crate serde_derive;
-use actix_web::{dev::Handler, http, server, App, Error, HttpRequest, HttpResponse, Responder};
+//use actix_web::{dev::Handler, http, server, App, Error, HttpRequest, HttpResponse, Responder};
+use actix_web::*;
+use actix_web::{dev::Handler};
 use listenfd::ListenFd;
 use std::cell::Cell;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
+use bytes::Bytes;
+use futures::stream::once;
+use futures::future::{Future, result};
 
 struct AppState {
     counter: Cell<usize>,
@@ -44,6 +49,17 @@ impl Responder for MyObj {
     }
 }
 
+#[derive(Deserialize)]
+struct Info {
+    userid2: u32,
+    friend2: String,
+}
+
+#[derive(Deserialize)]
+struct QueryInfo {
+    username: String,
+}
+
 fn index(req: &HttpRequest<AppState>) -> impl Responder {
     let count = req.state().counter.get() + 1;
     req.state().counter.set(count);
@@ -53,6 +69,67 @@ fn index(req: &HttpRequest<AppState>) -> impl Responder {
 
 fn json_test(_req: &HttpRequest<AppState>) -> impl Responder {
     MyObj { name: "小牛哥"}
+}
+
+fn async_test1(_req: &HttpRequest<AppState>) -> Box<Future<Item=HttpResponse, Error=Error>> {
+    result(Ok(HttpResponse::Ok()
+              .content_type("text/html")
+              .body(format!("Hello async xiaoniuge!"))))
+           .responder()
+}
+
+fn async_test2(_req: &HttpRequest<AppState>) -> Box<Future<Item=&'static str, Error=Error>> {
+    result(Ok("welcome async2 xiaoniuge")).
+        responder()
+}
+
+fn async_test3(_req: &HttpRequest<AppState>) -> HttpResponse {
+    let body = once(Ok(Bytes::from_static(b"test async xiaoniuge 3")));
+
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .body(Body::Streaming(Box::new(body)))
+}
+
+fn error_test(_req: &HttpRequest<AppState>) -> Result<Box<Future<Item=HttpResponse, Error=Error>>, Error> {
+    //if true {
+        Err(error::ErrorBadRequest("bad request"))        
+    // } else {
+        // Ok(
+        //     Box::new(
+        //         result(
+        //             Ok(HttpResponse::Ok().
+        //             content_type("text/html").
+        //             body(format!("hello!")))
+        //         )
+        //     )
+        // )
+    // }
+}
+
+fn path_test(/*_req &HttpRequest<AppState>*/ info: Path<(u32, String)>) -> Result<String> {
+    Ok(format!("welcome {} {}", info.1, info.0))
+}
+
+fn descrialize_test(info: Path<Info>) -> Result<String> {
+    Ok(format!("{} welcome {}", info.userid2, info.friend2))
+}
+
+fn query_test(info: Query<QueryInfo>) -> String {
+    format!("Welcome {}", info.username)
+}
+
+// #[derive(Deserialize)]
+// struct FormData {
+//     username: String,
+// }
+
+// fn form_test(form: Form<FormData>) -> Result<String> {
+//     Ok(format!("Welcome {}!", form.username))
+// }
+
+fn path_and_query((path, query): (Path<(u32, String)>, Query<QueryInfo>)) -> String {
+    format!("Welcome {} {} {}", query.username, path.0, path.1)
 }
 
 fn main() {
@@ -66,12 +143,24 @@ fn main() {
             .resource("/", |r| r.method(http::Method::GET).f(index))
             .resource("/inc-test", move |r| r.h(MyHandler(inc_cloned)))
             .resource("/json-test", |r| r.method(http::Method::GET).f(json_test))
+            .resource("/async-test1", |r| r.route().a(async_test1))
+            .resource("/async-test2", |r| r.route().a(async_test2))
+            .resource("/async-test3", |r| r.f(async_test3))
+            .resource("/error-test", |r| r.f(error_test))
+            .resource("/users/{userid}/{friend}", |r| r.method(http::Method::GET).with(path_test))
+            //http://127.0.0.1:9888/users2/2/xianiuge
+            .resource("/users2/{userid2}/{friend2}", |r| r.method(http::Method::GET).with(descrialize_test))
+            //http://127.0.0.1:9888/query-test?username=xiaoniuge
+            .resource("/query-test", |r| r.method(http::Method::GET).with(query_test))
+            .resource("/form-test", |r| r.method(http::Method::GET).with(query_test))
+            //http://127.0.0.1:9888/helloquery/444/xxxxx?username=xxx
+            .resource("/helloquery/{userid}/{friend}", |r| r.method(http::Method::GET).with(path_and_query))
     });
 
     server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
         server.listen(l)
     } else {
-        server.bind("127.0.0.1:8888").unwrap()
+        server.bind("127.0.0.1:9888").unwrap()
     };
 
     server.run();
